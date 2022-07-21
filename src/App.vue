c7841a801261a83913fec77c5a308cc513cc0ad1
Millefeuille <mlabouri@student.42.fr>
2022-07-21 10:06:09 +0200

    prout

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  	<div id="app">
+		<NavBar/>
+		<h1>PDF: Pong De Fou</h1>
+  	</div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import NavBar from "@/components/NavBar.vue";
 
 @Component({
   components: {
-    HelloWorld,
+	  NavBar
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+body {
+	margin: 0;
 }
+
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+}
+
+#big-picture img {
+	max-width: 100%;
+	max-height: 100%;
+	object-fit: contain;
+}
+
 </style>
---
+++ src/components/NavLink.vue
@@ -0,0 +1,20 @@
+<template>
+  <a :href='url'>{{ text }}</a>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+
+@Component({
+	props: {
+		url: String,
+		text: String
+	}
+})
+export default class NavLink extends Vue {}
+
+</script>
+
+<style scoped>
+
+</style>
