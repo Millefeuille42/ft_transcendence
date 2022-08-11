bbd24b16cce473c6ade34a0fb4a679cb629b3683
Cristalia <ericard@student.42.fr>
2022-08-11 19:30:43 +0200

    fix(middleware): add verification and authorize only localhost and front

--- backend/back_trans/src/middlewares/app-logger.middleware.ts
+++ backend/back_trans/src/middlewares/app-logger.middleware.ts
@@ -1,4 +1,4 @@
-import { Injectable, NestMiddleware, Logger, ForbiddenException} from '@nestjs/common';
+import {Injectable, NestMiddleware, Logger, ForbiddenException, HttpException, HttpStatus} from '@nestjs/common';
 import { Request, Response } from "express";
 
 @Injectable()
@@ -7,12 +7,14 @@ export class AppLoggerMiddleware implements NestMiddleware {
 
   use(req: Request, res: Response, next: () => void) {
     const {headers, method, originalUrl: url} = req;
+    //::ffff:10.11.12.1
+    if (headers["origin"] !== 'http://e1r12p1:8080' && req.ip !== "::ffff:127.0.0.1") {
+      throw new ForbiddenException()
+    }
 
     res.on('close', () => {
       const {statusCode} = res;
 
-      //if (headers["origin"] !== 'http://e1r12p1:8080')
-        //throw new ForbiddenException();
       this.logger.log(`${method} ${url} ${statusCode} - ${headers["origin"]}`)
     });
 
