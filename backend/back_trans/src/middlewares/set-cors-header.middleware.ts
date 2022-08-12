aeaafd59f15a8e75ae656043aacbbfe242e26881
Millefeuille <mlabouri@student.42.fr>
2022-08-12 22:34:21 +0200

    fix(middleware): added cors headers for DELETE method

--- backend/back_trans/src/middlewares/set-cors-header.middleware.ts
+++ backend/back_trans/src/middlewares/set-cors-header.middleware.ts
@@ -5,7 +5,7 @@ import {Request, Response} from 'express';
 export class SetCorsHeaderMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: () => void) {
     res.set('Access-Control-Allow-Origin', req.headers["origin"])
-    res.set('Access-Control-Allow-Methods', "PATCH")
+    res.set('Access-Control-Allow-Methods', ["PATCH, DELETE"])
     res.set('Access-Control-Allow-Credentials', "true")
     res.set('Access-Control-Allow-Headers', "content-type")
     if (req.method === "OPTIONS") {
