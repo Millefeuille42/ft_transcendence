11a75b29345ba856ee9693d7990efdde1b487271
Cristalia <ericard@student.42.fr>
2022-08-12 16:30:03 +0200

    fix(middleware): authorize postman access in back

--- backend/back_trans/src/middlewares/app-logger.middleware.ts
+++ backend/back_trans/src/middlewares/app-logger.middleware.ts
@@ -8,15 +8,21 @@ export class AppLoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: () => void) {
     const {headers, method, originalUrl: url} = req;
     //::ffff:10.11.12.1
-    if (headers["origin"] !== 'http://e1r11p2:8080' && req.ip !== "::ffff:127.0.0.1") {
-      throw new ForbiddenException()
+    if (headers["origin"] !== 'http://e1r11p2:8080' && req.ip !== "::ffff:127.0.0.1" &&
+        req.ip !== "::1") /*Postman*/ {
+      res.statusCode = HttpStatus.FORBIDDEN
     }
-
     res.on('close', () => {
       const {statusCode} = res;
 
       this.logger.log(`${method} ${url} ${statusCode} - ${headers["origin"]}`)
     });
+
+    if (headers["origin"] !== 'http://e1r11p2:8080' && req.ip !== "::ffff:127.0.0.1" &&
+        req.ip !== "::1") /*Postman*/ {
+      res.statusCode = HttpStatus.FORBIDDEN
+      throw new ForbiddenException()
+    }
 
     next();
   }
