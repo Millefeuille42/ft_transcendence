c266275ef086d1a7323c7cabf5402f4f4d68d818
Cristalia <ericard@student.42.fr>
2022-08-30 14:30:46 +0200

    fix(middleware): return 401 error when user is not logged

--- backend/back_trans/src/middlewares/isAuth.middleware.ts
+++ backend/back_trans/src/middlewares/isAuth.middleware.ts
@@ -1,4 +1,4 @@
-import { Injectable, NestMiddleware} from '@nestjs/common';
+import {ForbiddenException, HttpException, HttpStatus, Injectable, NestMiddleware} from '@nestjs/common';
 import {Request, Response} from 'express';
 import {UserService} from "../user/user.service";
 import {ConfigService} from "@nestjs/config";
@@ -34,30 +34,29 @@ export class IsAuthMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: () => void) {
     const redir: string = this.configService.get<string>('HOST') + ':' + this.configService.get<string>('PORT');
     const login: string = req.cookies['Session'];
-    if (req.ip === "::ffff:127.0.0.1" || req.ip === "::1") {
-      console.log("from localhost")
-      next();
-      return ;
-    }
+    //if (req.ip === "::ffff:127.0.0.1" || req.ip === "::1") {
+     // console.log("from localhost")
+      //next();
+      //return ;
+    //}
+    console.log(login)
     if (!login) {
       console.log("No login")
-      res.redirect(redir + '/auth');
-      return ;
+      res.statusCode = 401
+      throw new HttpException("User not logged", 401) ;
     }
     const token: string = this.userService.getToken(login);
     if (!token) {
       console.log('Pas dans la base de donnee')
-      res.clearCookie('Session');
-      res.redirect(redir + '/auth');
-      return ;
+      res.statusCode = 401
+      throw new HttpException("User not logged", 401) ;
     }
     const ret: boolean = await this.meRequest(token);
     if (!ret) {
       console.log(ret)
       console.log('Token qui fonctionne pas')
-      res.clearCookie('Session');
-      res.redirect(redir + '/auth');
-      return ;
+      res.statusCode = 401
+      throw new HttpException("User not logged", 401) ;
     }
     next();
   }
