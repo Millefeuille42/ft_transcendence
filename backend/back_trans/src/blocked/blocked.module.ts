d471be8c33b7131f4eb9f9b20b19453f0e16593a
Cristalia <ericard@student.42.fr>
2022-08-30 17:14:42 +0200

    fix(controller): import modules

--- backend/back_trans/src/blocked/blocked.module.ts
+++ backend/back_trans/src/blocked/blocked.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BlockedController } from './blocked.controller';
 import { BlockedService } from './blocked.service';
 import {UserModule} from "../user/user.module";
+import {TmpDbModule} from "../tmp_db/tmp_db.module";
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, TmpDbModule],
   controllers: [BlockedController],
   providers: [BlockedService]
 })
