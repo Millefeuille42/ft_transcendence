d8b27ffa12dd68140a90043a649abbca452e2eb0
Cristalia <ericard@student.42.fr>
2022-08-30 18:15:38 +0200

    feat(module): add circular dependency in friend and blocked modules

--- backend/back_trans/src/blocked/blocked.module.ts
+++ backend/back_trans/src/blocked/blocked.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import { BlockedController } from './blocked.controller';
 import { BlockedService } from './blocked.service';
 import {UserModule} from "../user/user.module";
 import {TmpDbModule} from "../tmp_db/tmp_db.module";
+import {FriendsModule} from "../friends/friends.module";
 
 @Module({
-  imports: [UserModule, TmpDbModule],
+  imports: [UserModule, TmpDbModule, forwardRef(() => FriendsModule)],
   controllers: [BlockedController],
-  providers: [BlockedService]
+  providers: [BlockedService],
+  exports: [BlockedService]
 })
 export class BlockedModule {}
