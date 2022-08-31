912c8eb2e4f2751025c071204c0eac584a66cbe1
Cristalia <ericard@student.42.fr>
2022-08-31 11:08:10 +0200

    feat(service): Add function to get stats and add stats

--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -20,25 +20,60 @@ export class GameService {
 	initStats(): stats {
 		let stats: stats = {
 			total: 0,
-			win: 0,
-			loose: 0,
+			wins: 0,
+			looses: 0,
 			points: 5,
 			lastRival: 'No one :(',
 		}
 		return(stats);
 	}
 
+	getStats(login: string) {
+		const user = this.userService.getUser(login)
+		return user.stats
+	}
+
+	getWins(login: string) {
+		const user = this.userService.getUser(login)
+		return {wins: user.stats.wins}
+	}
+
+	getLooses(login: string) {
+		const user = this.userService.getUser(login)
+		return {looses: user.stats.looses}
+	}
+
+	getTotal(login: string) {
+		const user = this.userService.getUser(login)
+		return {total: user.stats.total}
+	}
+
+	getPoints(login: string) {
+		const user = this.userService.getUser(login)
+		return {points: user.stats.points}
+	}
+
+	getLastRival(login: string) {
+		const user = this.userService.getUser(login)
+		return {lastRival: user.stats.lastRival}
+	}
+
 	addStats(login: string, result: string, rival: string) {
 		this.verificationUsers(login, rival)
-
+		let user = this.userService.getUser(login);
 
 		if (result === 'win') {
-
+			user.stats.wins++
+			user.stats.points += 2
 		}
 		else if (result === 'loose') {
-
+			user.stats.looses++
+			user.stats.points++
 		}
 		else
 			throw new HttpException('Result must be a win or a loose', HttpStatus.BAD_REQUEST)
+		user.stats.total++
+		user.stats.lastRival = rival
+		return user.stats
 	}
 }
