85abf1814d577978e54d29916bdd6608fb039f6f
Cristalia <ericard@student.42.fr>
2022-08-31 10:49:03 +0200

    All node files useless removed

--- backend/back_trans/src/game/game.controller.ts
+++ backend/back_trans/src/game/game.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Param, Post} from '@nestjs/common';
+import {GameService} from "./game.service";
 
 @Controller('game')
-export class GameController {}
+export class GameController {
+	constructor(readonly private gameService: GameService) {}
+
+	@Post(':login')
+	addStats(@Param('login') login,
+			 @Body() result: string,
+			 @Body() rival: string) {
+
+	}
+}
