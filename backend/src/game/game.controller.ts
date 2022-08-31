44de179207003117b23a8365dc499158a00d2a80
Cristalia <ericard@student.42.fr>
2022-08-31 11:51:35 +0200

    feat(service): Controller for get and change stats of a user

--- backend/src/game/game.controller.ts
+++ backend/src/game/game.controller.ts
@@ -1,14 +1,37 @@
-import {Body, Controller, Param, Post} from '@nestjs/common';
+import {Body, Controller, Get, Param, Patch, Post} from '@nestjs/common';
 import {GameService} from "./game.service";
+import {AddStatsDto} from "./add-stats.dto";
 
 @Controller('game')
 export class GameController {
-	constructor(readonly private gameService: GameService) {}
+	constructor(private readonly gameService: GameService) {}
+
+	@Get(':login/:resource')
+	getStats(@Param('login') login: string,
+			 @Param('resource') resource: string) {
+		if (resource === 'stats')
+			return (this.gameService.getStats(login))
+		if (resource === 'wins')
+			return this.gameService.getWins(login)
+		if (resource === 'looses')
+			return this.gameService.getLooses(login)
+		if (resource === 'total')
+			return this.gameService.getTotal(login)
+		if (resource === 'points')
+			return this.gameService.getPoints(login)
+		if (resource === 'rival')
+			return this.gameService.getLastRival(login)
+	}
+
+	@Patch('points/:login')
+	fixPoints(@Param('login') login: string,
+			  @Body() points: number) {
+		return this.gameService.fixPoints(login, points)
+	}
 
 	@Post(':login')
-	addStats(@Param('login') login,
-			 @Body() result: string,
-			 @Body() rival: string) {
-
+	addStats(@Param('login') login: string,
+			 @Body() stats: AddStatsDto) {
+		return this.gameService.addStats(login, stats.win, stats.rival)
 	}
 }
