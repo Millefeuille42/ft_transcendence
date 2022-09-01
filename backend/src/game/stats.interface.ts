cdeb5864fee69ee473d6a2d639d4d884c30689d7
Cristalia <ericard@student.42.fr>
2022-09-01 10:05:19 +0200

    feat(interface): add history games in stats

--- backend/src/game/stats.interface.ts
+++ backend/src/game/stats.interface.ts
@@ -1,7 +1,15 @@
+export interface history {
+	rival: string,
+	userPoints: number,
+	rivalPoints: number,
+	gameMode: string,
+}
+
 export interface stats {
 	total: number,
 	wins: number,
 	looses: number,
 	points: number,
 	lastRival: string,
+	history: history[],
 }
