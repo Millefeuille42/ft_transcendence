8e5f37ece34830fdbe290c41736478a3f4cb8a5d
Cristalia <ericard@student.42.fr>
2022-09-06 18:31:52 +0200

    fix: friends - blocked list

--- backend/src/friends/friends.service.ts
+++ backend/src/friends/friends.service.ts
@@ -62,7 +62,7 @@ export class FriendsService {
 		if (await this.blockedService.isBlocked(friend, login))
 			throw new HttpException("Friend blocked User", HttpStatus.FORBIDDEN)
 		if (alreadyRelation)
-			relation = await this.relationsRepository.preload({id: user.id, otherLogin: friend, friend: true})
+			relation = await this.relationsRepository.preload({relationId: alreadyRelation.relationId, friend: true})
 		else
 			relation = {
 				id: user.id,
@@ -82,7 +82,7 @@ export class FriendsService {
 		let alreadyRelation = await this.relationsRepository.findOneBy({id: user.id, otherLogin: friend})
 		if (!alreadyRelation || (alreadyRelation && alreadyRelation.friend === false))
 			throw new BadRequestException()
-		let relation = await this.relationsRepository.preload({id: user.id, otherLogin: friend, friend: false})
+		let relation = await this.relationsRepository.preload({relationId: alreadyRelation.relationId, friend: false})
 		return await this.relationsRepository.save(relation)
 	}
 
--- scripts/initUsersForTests.sh
+++ scripts/initUsersForTests.sh
@@ -11,3 +11,101 @@ curl --location --request POST 'localhost:3000/user/' \
 --data-urlencode 'email=tester@letest.com' \
 --data-urlencode 'name=Le Test' \
 --data-urlencode 'avatar=https://picsum.photos/200/200?random'
+
+curl --location --request POST 'localhost:3000/game/patate' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'rival=tester' \
+--data-urlencode 'points=0' \
+--data-urlencode 'rPoints=5' \
+--data-urlencode 'mode=Normal'
+
+curl --location --request POST 'localhost:3000/game/patate' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'rival=tester' \
+--data-urlencode 'points=5' \
+--data-urlencode 'rPoints=2' \
+--data-urlencode 'mode=Normal'
+
+curl --location --request POST 'localhost:3000/game/patate' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'rival=tester' \
+--data-urlencode 'points=2' \
+--data-urlencode 'rPoints=5' \
+--data-urlencode 'mode=White_Mode'
+
+curl --location --request POST 'localhost:3000/game/patate' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'rival=tester' \
+--data-urlencode 'points=5' \
+--data-urlencode 'rPoints=4' \
+--data-urlencode 'mode=Normal'
+
+curl --location --request POST 'localhost:3000/game/patate' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'rival=tester' \
+--data-urlencode 'points=5' \
+--data-urlencode 'rPoints=3' \
+--data-urlencode 'mode=Normal'
+
+curl --location --request POST 'localhost:3000/game/patate' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'rival=tester' \
+--data-urlencode 'points=5' \
+--data-urlencode 'rPoints=0' \
+--data-urlencode 'mode=White_Mode'
+
+curl --location --request POST 'localhost:3000/game/patate' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'rival=tester' \
+--data-urlencode 'points=5' \
+--data-urlencode 'rPoints=1' \
+--data-urlencode 'mode=Normal'
+
+curl --location --request POST 'localhost:3000/game/patate' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'rival=tester' \
+--data-urlencode 'points=5' \
+--data-urlencode 'rPoints=4' \
+--data-urlencode 'mode=Normal'
+
+curl --location --request POST 'localhost:3000/game/patate' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'rival=tester' \
+--data-urlencode 'points=5' \
+--data-urlencode 'rPoints=2' \
+--data-urlencode 'mode=Normal'
+
+curl --location --request POST 'localhost:3000/game/patate' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'rival=tester' \
+--data-urlencode 'points=4' \
+--data-urlencode 'rPoints=5' \
+--data-urlencode 'mode=Normal'
+
+curl --location --request POST 'localhost:3000/game/patate' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'rival=tester' \
+--data-urlencode 'points=0' \
+--data-urlencode 'rPoints=5' \
+--data-urlencode 'mode=Normal'
+
+curl --location --request POST 'localhost:3000/game/patate' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'rival=tester' \
+--data-urlencode 'points=5' \
+--data-urlencode 'rPoints=1' \
+--data-urlencode 'mode=Normal'
+
+curl --location --request POST 'localhost:3000/game/patate' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'rival=tester' \
+--data-urlencode 'points=3' \
+--data-urlencode 'rPoints=5' \
+--data-urlencode 'mode=Normal'
+
+curl --location --request POST 'localhost:3000/game/patate' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'rival=tester' \
+--data-urlencode 'points=5' \
+--data-urlencode 'rPoints=2' \
+--data-urlencode 'mode=Normal'
